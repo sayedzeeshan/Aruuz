ecc03127f3ffafea0a6fe9ffea8a0e2699275e90
Sawood Alam <ibnesayeed@gmail.com>
2015-02-01 21:46:50 -0500

    Refactoring StateMachine class

--- Aruuz.Website/Models/StateMachine.cs
+++ Aruuz.Website/Models/StateMachine.cs
@@ -7,201 +7,50 @@ namespace Aruuz.Models
 {
     public class StateMachine
     {
-        static public int HindiMeter(string input, int state)
+        static private readonly Dictionary<string, int[]> hindiMeterTransition = new Dictionary<string, int[]>
+        {
+            {"-", new int[] {2, 4, -1, 0, 5, -1, 7, -1}},
+            {"=", new int[] {1, 0, 3, -1, 6, 1, -1, 0}}
+        };
+
+        static private readonly Dictionary<string, int[]> zamzamaMeterTransition = new Dictionary<string, int[]>
+        {
+            {"-", new int[] {1, 2, -1, -1}},
+            {"=", new int[] {3, -1, 0, 0}}
+        };
+
+        static private readonly Dictionary<string, int[]> originalHindiMeterTransition = new Dictionary<string, int[]>
+        {
+            {"-", new int[] {-1, 2, 3, -1}},
+            {"=", new int[] {1, 0, -1, 1}}
+        };
+
+        static private int NextState(Dictionary<string, int[]> transition, string input, int state)
         {
             int nextState = -1;
-            if (input.Equals("-"))
+            try
             {
-                if (state == 0)
-                {
-                    state = 2;
-                    nextState = state;
-                }
-                else if (state == 1)
-                {
-                    state = 4;
-                    nextState = state;
-                }
-                else if (state == 2)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 3)
-                {
-                    state = 0;
-                    nextState = state;
-                }
-                else if (state == 4)
-                {
-                    state = 5;
-                    nextState = state;
-                }
-                else if (state == 5)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 6)
-                {
-                    state = 7;
-                    nextState = state;
-                }
-                else if (state == 7)
-                {
-                    state = -1;
-                    nextState = state;
-                }
+                nextState = transition[input][state];
             }
-            else if (input.Equals("="))
+            finally
             {
-                if (state == 0)
-                {
-                    state = 1;
-                    nextState = state;
-                }
-                else if (state == 1)
-                {
-                    state = 0;
-                    nextState = state;
-                }
-                else if (state == 2)
-                {
-                    state = 3;
-                    nextState = state;
-                }
-                else if (state == 3)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 4)
-                {
-                    state = 6;
-                    nextState = state;
-                }
-                else if (state == 5)
-                {
-                    state = 1;
-                    nextState = state;
-                }
-                else if (state == 6)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 7)
-                {
-                    state = 0;
-                    nextState = state;
-                }
+                return nextState;
             }
-            return nextState;
+        }
+
+        static public int HindiMeter(string input, int state)
+        {
+            return NextState(hindiMeterTransition, input, state);
         }
 
         static public int ZamzamaMeter(string input, int state)
         {
-            int nextState = -1;
-            if (input.Equals("-"))
-            {
-                if (state == 0)
-                {
-                    state = 1;
-                    nextState = state;
-                }
-                else if (state == 1)
-                {
-                    state = 2;
-                    nextState = state;
-                }
-                else if (state == 2)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 3)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-            }
-            else if (input.Equals("="))
-            {
-                if (state == 0)
-                {
-                    state = 3;
-                    nextState = state;
-                }
-                else if (state == 1)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 2)
-                {
-                    state = 0;
-                    nextState = state;
-                }
-                else if (state == 3)
-                {
-                    state = 0;
-                    nextState = state;
-                }
-            }
-            return nextState;
+            return NextState(zamzamaMeterTransition, input, state);
         }
 
         static public int OriginalHindiMeter(string input, int state)
         {
-            int nextState = -1;
-            if (input.Equals("-"))
-            {
-                if (state == 0)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 1)
-                {
-                    state = 2;
-                    nextState = state;
-                }
-                else if (state == 2)
-                {
-                    state = 3;
-                    nextState = state;
-                }
-                else if (state == 3)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-            }
-            else if (input.Equals("="))
-            {
-                if (state == 0)
-                {
-                    state = 1;
-                    nextState = state;
-                }
-                else if (state == 1)
-                {
-                    state = 0;
-                    nextState = state;
-                }
-                else if (state == 2)
-                {
-                    state = -1;
-                    nextState = state;
-                }
-                else if (state == 3)
-                {
-                    state = 1;
-                    nextState = state;
-                }
-            }
-            return nextState;
-
+            return NextState(originalHindiMeterTransition, input, state);
         }
     }
 }
